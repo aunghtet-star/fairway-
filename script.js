ff833e537910f659b39f48bc16b2f0442cf338af
Huchi Man <aunghtetthu172@gmail.com>
2021-04-30 12:05:40 +0630

    add index

---
+++ script.js
@@ -0,0 +1,12 @@
+        function MgMg(){
+            document.getElementById('container').innerHTML='Hello MG MG';
+        }
+        function yeLlow(){
+            document.getElementById('container').style.color='red';
+        }
+        function font(){
+            document.getElementById('container').style.fontSize='30px';
+        }
+        console.log("I'm a coder");
+        document.write("I'm a coder");
+        document.getElementById('container').innerHTML="I'm a coder";
